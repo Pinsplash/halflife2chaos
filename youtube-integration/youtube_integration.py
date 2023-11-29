0e0ecddf13110a76d0a737a7df1aa84fe0a3445f
holy-jesus <hj@holy-coder.ru>
2023-11-29 16:48:29 +0500

    Added setup.bat for YouTube integration, updated the version of rcon in equirements.txt, and modified youtube_integration to use threads instead of async tasks.

--- youtube-integration/youtube_integration.py
+++ youtube-integration/youtube_integration.py
@@ -1,15 +1,24 @@
-import asyncio
 import threading
 from collections import Counter
 from typing import Optional
+import time
+from queue import Queue, Empty
 
-from pytchat_fix import LiveChatAsync, Processor, Message, extract_video_id, is_stream_live
-# noinspection PyUnresolvedReferences
-import obspython as obs
-from rcon.source import rcon
+from pytchat.core import PytchatCore
+from rcon.source import Client
 from rcon.exceptions import WrongPassword
 
-STREAM_ID = ""
+# noinspection PyUnresolvedReferences
+import obspython as obs
+
+from pytchat_fix import (
+    Processor,
+    Message,
+    extract_video_id,
+    is_chat_enabled,
+)
+
+STREAM_ID = None
 SOURCE_NAME = ""
 RCON_HOST = "127.0.0.1"
 RCON_PORT = "27015"
@@ -21,25 +30,20 @@ votes = {}
 voteEffects = []
 voteKeywords = []
 
-async def update_game_votes():
+
+def update_game_votes(rcon):
     global voteNumber, voteEffects, votes
     vote_counts = Counter(votes.values())
     vote_params = [""] * len(voteEffects)
     for i in range(len(voteEffects)):
         vote_params[i] = str(vote_counts.get(i) or 0)
-    await rcon(
-        'chaos_vote_internal_set', str(voteNumber), *vote_params,
-        host=RCON_HOST, port=int(RCON_PORT), passwd=RCON_PASSWORD
-    )
+    rcon.run("chaos_vote_internal_set", str(voteNumber), *vote_params)
 
 
-async def poll_game():
+def poll_game(rcon):
     global voteNumber, voteEffects, votes
-    raw_resp = await rcon(
-        'chaos_vote_internal_poll',
-        host=RCON_HOST, port=int(RCON_PORT), passwd=RCON_PASSWORD
-    )
-    response = raw_resp.split("rcon from \"", 1)[0].strip()
+    raw_resp = rcon.run("chaos_vote_internal_poll")
+    response = raw_resp.split('rcon from "', 1)[0].strip()
     if response == "":
         return False  # the game is not quite ready yet.
 
@@ -50,22 +54,29 @@ async def poll_game():
         voteNumber = vote_number
         voteEffects = effects
         votes = {}
-
     return True
 
 
-async def game_loop():
+def game_loop():
+    STARTUP.wait()
     already_printed_err = False
     faulty_password = ""
+    rcon = None
     while True:
-        await asyncio.sleep(1)
+        if SHUTDOWN:
+            return
+
+        time.sleep(1)
         if faulty_password == RCON_PASSWORD:
             continue  # wait for the user to change password
         else:
             faulty_password = ""
         try:
-            if await poll_game():
-                await update_game_votes()
+            if rcon is None:
+                rcon = Client(RCON_HOST, int(RCON_PORT), passwd=RCON_PASSWORD)
+                rcon.connect(True)
+            if poll_game(rcon):
+                update_game_votes(rcon)
             if already_printed_err:
                 print("poll resumed as normal.")
             already_printed_err = False
@@ -73,67 +84,105 @@ async def game_loop():
             if not already_printed_err:
                 print("poll failed", e)
                 already_printed_err = True
+            rcon = None
         except WrongPassword:
             print("rcon wrong password")
             faulty_password = RCON_PASSWORD
+            rcon = None
         except Exception as e:
-            print("poll unexpected exception:", e)  # i broke something. log and bail
+            # traceback.print_exception(e)
+            if not already_printed_err:
+                print(
+                    "poll unexpected exception:", e
+                )  # i broke something. log and bail
+                already_printed_err = True
+            rcon = None
+            # return
+
+
+STARTUP = threading.Event()
+CAN_CONNECT = threading.Event()
+CAN_CONNECT.set()
+SHUTDOWN = False
+chat: Optional[PytchatCore] = None
+game_thread: Optional[threading.Thread] = None
+chat_thread: Optional[threading.Thread] = None
+
+
+def channel_loop():
+    global STREAM_ID, chat, chat_thread
+    try:
+        while True:
+            new_stream_id = channel_queue.get(timeout=2)
+            channel_queue.task_done()
+    except Empty:
+        if chat:
+            chat.terminate()
+            chat_thread.join()
+            chat = None
+        STREAM_ID = new_stream_id
+        CAN_CONNECT.clear()
+        chat_thread = threading.Thread(None, chat_loop, daemon=False)
+        chat_thread.start()
+
+
+channel_queue = Queue()
+channel_thread: Optional[threading.Thread] = None
+
+
+def chat_loop():
+    global chat
+    STARTUP.wait()
+    try:
+        if not STREAM_ID:
+            print(
+                "The provided URL seems to be incorrect or invalid. Please recheck the stream URL and try again."
+            )
+            chat = None
+            CAN_CONNECT.set()
             return
+        status = is_chat_enabled(STREAM_ID)
+        if status is False:
+            print(
+                "The provided URL doesn't seem to be a live stream, or chat for this stream is disabled. Please make sure you've entered a valid stream URL with an enabled chat."
+            )
+            chat = None
+            CAN_CONNECT.set()
+            return
+        elif status is None:
+            print(
+                "The provided URL seems to be incorrect or invalid. Please recheck the stream URL and try again."
+            )
+            chat = None
+            CAN_CONNECT.set()
+            return
+        chat = PytchatCore(STREAM_ID, processor=Processor(), interruptable=False)
+        while chat.is_alive():
+            if SHUTDOWN:
+                return
 
-chat: Optional[LiveChatAsync] = None
-chat_task: Optional[asyncio.Task] = None
-poll_task: Optional[asyncio.Task] = None
-channel_task: Optional[asyncio.Task] = None
-channel_queue = asyncio.Queue()    
+            chat_data = chat.get()
+            if not CAN_CONNECT.is_set():
+                print("connected")
+                CAN_CONNECT.set()
+            for message in chat_data.items:
+                message: Message = message
+                text = message.message.strip().upper()
+                print(f"{message.author.name} said: {message.message}")
+                uppercase_keywords = [kw.upper() for kw in voteKeywords]
+                if text in uppercase_keywords:
+                    votes[message.author.channelId] = uppercase_keywords.index(text)
+            time.sleep(1.5)
 
-async def chat_loop():
-    global chat, votes, chat_task
-    if not STREAM_ID:
-        print("incorrect video id, please check stream url")
-        chat = None
-        chat_task = None
-        return
-    status = await is_stream_live(STREAM_ID)
-    if status is False:
-        print("Stream offline or chat disabled, please, check stream url")
-        chat = None
-        chat_task = None
-        return
-    elif status is None:
-        print("incorrect video id, please check stream url")
-        chat = None
-        chat_task = None
-        return
-    chat = await LiveChatAsync(STREAM_ID, processor=Processor())
-    while chat.is_alive():
-        async for message in (await chat.get()).async_items():
-            message: Message = message
-            print(f'{message.author.name} said: {message.message}')
-            uppercase_keywords = [kw.upper() for kw in voteKeywords]
-            if message.message.upper() in uppercase_keywords:
-                votes[message.author.channelId] = uppercase_keywords.index(message.message.upper())
-
-async def startup():
-    global poll_task, chat_task
-    # since we're running in a thread, we need to wait for obs to set our properties.
-    # TODO: change this to a future or some other kind of awaitable?
-    await asyncio.sleep(1)
-    loop = asyncio.get_event_loop()
-    poll_task = loop.create_task(game_loop())
-    chat_task = loop.create_task(chat_loop())
-    print("startup done")
-
-
-async def shutdown():
-    global chat, poll_task
-    if poll_task:
-        poll_task.cancel()
-        poll_task = None
-    if chat_task:
-        chat_task.cancel()
+    except Exception as e:
+        print(
+            "Error in chat happened. ",
+            e,
+            "\nTo start chat again press button 'Reconnect to youtube'.",
+        )
         chat.terminate()
         chat = None
-    print("shutdown done")
+        CAN_CONNECT.set()
 
 
 def set_text(source: str, text: str):
@@ -152,45 +201,23 @@ def update_source():
     output = f"Vote #{voteNumber}\n"
     vote_counts = Counter(votes.values())
     for i, voteEffect in enumerate(voteEffects):
-        keyword = '?' if i > len(voteKeywords) - 1 else voteKeywords[i]
+        keyword = "?" if i > len(voteKeywords) - 1 else voteKeywords[i]
         vote_count = vote_counts.get(i) or 0
         output = output + f"{keyword} {voteEffect}: {vote_count}\n"
     set_text(SOURCE_NAME, output[:-1])  # exclude final newline
 
-def channel_update(url: str):
-    global channel_task, chat_task, chat
-    video_id = extract_video_id(url)
-    if video_id and (not chat or chat._video_id != video_id):
-        if not channel_task or channel_task.done():
-            channel_task = _LOOP.create_task(channel_loop())
-        channel_queue.put_nowait(video_id)
-
-async def channel_loop():
-    global STREAM_ID, chat, chat_task
-    loop = asyncio.get_event_loop()
-    try:
-        while True:
-            video_id: str = await asyncio.wait_for(channel_queue.get(), 2)
-    except TimeoutError:
-        if video_id and (not chat or chat._video_id != video_id):
-            if chat:
-                chat.terminate()
-                chat = None
-            if chat_task:
-                chat_task.cancel()
-                chat_task = None
-            STREAM_ID = video_id
-            chat_task = loop.create_task(chat_loop())
-
-_LOOP: Optional[asyncio.AbstractEventLoop] = None
-_THREAD: Optional[threading.Thread] = None
 
 def script_properties():
     props = obs.obs_properties_create()
     obs.obs_properties_add_text(props, "stream_url", "Stream URL", obs.OBS_TEXT_DEFAULT)
 
-    p = obs.obs_properties_add_list(props, "source", "Text Source", obs.OBS_COMBO_TYPE_EDITABLE,
-                                    obs.OBS_COMBO_FORMAT_STRING)
+    p = obs.obs_properties_add_list(
+        props,
+        "source",
+        "Text Source",
+        obs.OBS_COMBO_TYPE_EDITABLE,
+        obs.OBS_COMBO_FORMAT_STRING,
+    )
     sources = obs.obs_enum_sources()
     if sources:
         for source in sources:
@@ -201,23 +228,37 @@ def script_properties():
     obs.source_list_release(sources)
 
     obs.obs_properties_add_text(props, "rcon_host", "RCON host", obs.OBS_TEXT_DEFAULT)
-    obs.obs_properties_add_text(props, "rcon_password", "RCON password", obs.OBS_TEXT_PASSWORD)
-    obs.obs_properties_add_editable_list(props, "vote_keywords", "Vote keywords", obs.OBS_EDITABLE_LIST_TYPE_STRINGS,
-                                         None, None)
-    async def reconnect():
-        print("reconnecting to twitch")
+    obs.obs_properties_add_text(
+        props, "rcon_password", "RCON password", obs.OBS_TEXT_PASSWORD
+    )
+    obs.obs_properties_add_editable_list(
+        props,
+        "vote_keywords",
+        "Vote keywords",
+        obs.OBS_EDITABLE_LIST_TYPE_STRINGS,
+        None,
+        None,
+    )
+
+    def reconnect(*args, **kwargs):
+        global chat_thread
+        print("reconnecting to youtube")
         chat.terminate()
-        await chat_loop()
+        chat_thread.join()
+        CAN_CONNECT.clear()
+        chat_thread = threading.Thread(None, chat_loop, daemon=False)
+        chat_thread.start()
 
-    def call_reconnect(props, p):
-        _LOOP.call_soon_threadsafe(lambda l: asyncio.ensure_future(reconnect()), _LOOP)
-
-    obs.obs_properties_add_button(props, "reconnect_button", "Reconnect to twitch", call_reconnect)
+    obs.obs_properties_add_button(
+        props, "reconnect_button", "Reconnect to youtube", reconnect
+    )
     return props
 
 
 def script_defaults(settings):
-    obs.obs_data_set_default_string(settings, "stream_url", "https://youtube.com/?v=")
+    obs.obs_data_set_default_string(
+        settings, "stream_url", "https://www.youtube.com/watch?v="
+    )
     obs.obs_data_set_default_string(settings, "rcon_host", "127.0.0.1:27015")
 
     obs_array = obs.obs_data_array_create()
@@ -232,9 +273,17 @@ def script_defaults(settings):
 
 def script_update(settings):
     # i feel like i'm doing something wrong.
-    global voteKeywords, STREAM_ID, SOURCE_NAME, RCON_HOST, RCON_PORT, RCON_PASSWORD
-    STREAM_ID = extract_video_id(obs.obs_data_get_string(settings, "stream_url"))
-    channel_update(STREAM_ID)
+    global voteKeywords, STREAM_ID, SOURCE_NAME, RCON_HOST, RCON_PORT, RCON_PASSWORD, channel_thread
+    new_stream_id = extract_video_id(obs.obs_data_get_string(settings, "stream_url"))
+    if not STARTUP.is_set():
+        STREAM_ID = new_stream_id
+    if STREAM_ID != new_stream_id:
+        CAN_CONNECT.wait(2.0)  # Prevents updates while the chat is starting
+        if not channel_thread or not channel_thread.is_alive():
+            channel_thread = threading.Thread(None, channel_loop)
+            channel_thread.start()
+        channel_queue.put(new_stream_id)
+        # STREAM_ID = stream_id
     SOURCE_NAME = obs.obs_data_get_string(settings, "source")
     # TODO: Verify port here. We may have an error if we don't
     RCON_HOST, RCON_PORT = obs.obs_data_get_string(settings, "rcon_host").split(":", 1)
@@ -249,48 +298,33 @@ def script_update(settings):
         obs.obs_data_release(item)
     obs.obs_data_array_release(obs_votes_keywords)
     print("data updated")
+    STARTUP.set()
 
-# https://gist.github.com/serializingme/5c1a6fd6c7ea58af77c7b80579737c5a
 
 def script_load(settings):
-    global _LOOP, _THREAD
-
-    # let's be nice, and only call the obs's methods from its own thread.
-    # TODO: call this every frame because why not?
+    global SHUTDOWN, STARTUP, game_thread, chat_thread
+    SHUTDOWN = False
+    STARTUP.clear()
     obs.timer_add(update_source, 1000)
-
-    _LOOP = asyncio.new_event_loop()
-
-    def async_thread():
-        global _LOOP
-        asyncio.set_event_loop(_LOOP)
-        asyncio.ensure_future(startup())
-        _LOOP.run_forever()
-        _LOOP.close()
-        _LOOP = None
-
-    _THREAD = threading.Thread(None, async_thread, daemon=True)
-    _THREAD.start()
+    game_thread = threading.Thread(None, game_loop, daemon=False)
+    game_thread.start()
+    CAN_CONNECT.clear()
+    chat_thread = threading.Thread(None, chat_loop, daemon=False)
+    chat_thread.start()
 
 
 def script_unload():
+    global game_thread, chat_thread, SHUTDOWN
     obs.timer_remove(update_source)
-
-    global _LOOP, _THREAD
-    if _LOOP is not None:
-        async def destroy():
-            await shutdown()
-            _LOOP.stop()
-
-        # let the destructor run
-        _LOOP.call_soon_threadsafe(lambda l: asyncio.ensure_future(destroy()), _LOOP)
-
-    if _THREAD is not None:
-        # Wait for 5 seconds, if it doesn't exit just move on not to block
-        # OBS main thread. Logging something about the failure to properly exit
-        # is advised.
-        _THREAD.join()
-        _THREAD = None
+    SHUTDOWN = True
+    for thread in (game_thread, chat_thread):
+        if thread is not None and thread.is_alive():
+            # Wait for 5 seconds, if it doesn't exit just move on not to block
+            # OBS main thread. Logging something about the failure to properly exit
+            # is advised.
+            thread.join(5.0)
+    game_thread = None
+    chat_thread = None
 
 
 def script_description():
